aa78b4110643dfed99d466add09d91495962a8be
swaga32 <jacksonmbirui3@gmail.com>
2023-06-19 17:56:35 +0000

    pol

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,20 @@
+import { createQueue } from 'kue';
+
+const queue = createQueue({name: 'push_notification_code'});
+
+const job = queue.create('push_notification_code', {
+  phoneNumber: '07039800800',
+  message: 'Account created',
+});
+
+job
+  .on('enqueue', () => {
+    console.log('Notification job created:', job.id);
+  })
+  .on('complete', () => {
+    console.log('Notification job completed');
+  })
+  .on('failed attempt', () => {
+    console.log('Notification job failed');
+  });
+job.save();
